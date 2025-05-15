1bbd795bf4e4b347ee4853d9de1884399ef0bb89
theonealgo <nmesghali@gmail.com>
2025-05-15 15:40:20 -0400

    Update LoginForm.tsx

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { signIn } from "next-auth/react";
 
-async function handleLogin(e) {
+async function handleLogin'e' {
   e.preventDefault();
   await signIn("credentials", {
     redirect: true,
